c14d05fcf13cd8e156eb30ba330c7c4a28925e9c
bissbotch <64175657+bissbotch@users.noreply.github.com>
2021-04-15 19:07:09 +0100

    Bug fixing

--- src/Main.py
+++ src/Main.py
@@ -1,52 +1,43 @@
-def getInputs():
-    num1 = input("Enter a number: \n")
-    num2 = input("Enter a number \n")
-    
-    return num1, num2
+# Calculator      
+class Calculator:
+    def addition(self, num1, num2):
+        return num1+num2
+            
+    def subtraction(self, num1, num2):
+        return num1-num2
 
-def CalcMenu(self, num1, num2):
+    def multiplication(self, num1, num2):
+        return num1*num2
+
+    def division(self, num1, num2):
+        return num1/num2
+
+# Main
+def Main():
+    num1 = int(input("Enter a number: \n"))
+    num2 = int(input("Enter a number \n"))
+    
+    calc = Calculator()
+    
     print("Welcome to the calculator :) \n\n")
     print("***** Calculator Menu ***** \n")
-    print ("1) Addition\n 2) Subtraction\n 3) Multiplication\n 4) Division \n")
+    print (" 1) Addition\n 2) Subtraction\n 3) Multiplication\n 4) Division \n")
     calcType = input("What type of calculation do you want to do? \n")
     
-    if calcType == 1:
-        Calculator.addition(self, num1, num2)
+    if calcType == "1":
+        print("Answer:", calc.addition(num1, num2))
     
-    elif calcType == 2:
-        Calculator.subtraction(self, num1, num2)
+    elif calcType == "2":
+        print("Answer:", calc.subtraction(num1, num2))
         
-    elif calcType == 3:
-        Calculator.multiplication(self, num1, num2)
+    elif calcType == "3":
+        print("Answer:", calc.multiplication(num1, num2))
         
-    elif calcType == 4:
-        Calculator.division(self, num1, num2)
+    elif calcType == "4":
+        print("Answer:", calc.division(num1, num2))
     
     else:
         print("Please enter a valid option\n")
-
-class Calculator:
-
-    def addition(self, num1, num2):
-        answer = num1 + num2
-        return answer
         
-    def subtraction(self, num1, num2):
-        answer = num1 - num2
-        return answer
-
-    def multiplication(self, num1, num2):
-        answer = num1 * num2
-        return answer
-
-    def division(self, num1, num2):
-        answer = num1 / num2
-        return answer
-    
-    
-def Main(self, num1, num2, answer):
-    getInputs()
-    CalcMenu(self, num1, num2)
-    
-    print("Answer: ", answer)
+Main()
     
