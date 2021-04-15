60dd711f1da05155917f3d1e2c9c4a730b4029ea
bissbotch <64175657+bissbotch@users.noreply.github.com>
2021-04-15 13:54:20 +0100

    Cleaned up and removed errors

--- src/Main.py
+++ src/Main.py
@@ -1,36 +1,52 @@
-from Calculator import addition
-from Calculator import subtraction
-from Calculator import multiplication
-from Calculator import division
-
 def getInputs():
     num1 = input("Enter a number: \n")
     num2 = input("Enter a number \n")
     
     return num1, num2
 
-def CalcMenu():
+def CalcMenu(self, num1, num2):
     print("Welcome to the calculator :) \n\n")
     print("***** Calculator Menu ***** \n")
     print ("1) Addition\n 2) Subtraction\n 3) Multiplication\n 4) Division \n")
     calcType = input("What type of calculation do you want to do? \n")
     
     if calcType == 1:
-        Calculator.addition()
+        Calculator.addition(self, num1, num2)
     
     elif calcType == 2:
-        Calculator.subtraction()
+        Calculator.subtraction(self, num1, num2)
         
     elif calcType == 3:
-        Calculator.multiplication()
+        Calculator.multiplication(self, num1, num2)
         
     elif calcType == 4:
-        Calculator.division()
+        Calculator.division(self, num1, num2)
     
     else:
         print("Please enter a valid option\n")
 
-def Main():
+class Calculator:
+
+    def addition(self, num1, num2):
+        answer = num1 + num2
+        return answer
+        
+    def subtraction(self, num1, num2):
+        answer = num1 - num2
+        return answer
+
+    def multiplication(self, num1, num2):
+        answer = num1 * num2
+        return answer
+
+    def division(self, num1, num2):
+        answer = num1 / num2
+        return answer
+    
+    
+def Main(self, num1, num2, answer):
     getInputs()
-    CalcMenu()
+    CalcMenu(self, num1, num2)
+    
+    print("Answer: ", answer)
     
